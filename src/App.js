620e9b5761fa2045df22566b55bdfdfae001d5df
Nicolás Delgado Abad <nicolas.delgado@altia.es>
2020-04-06 12:34:36 +0200

    First example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import CreateForm from "./components/createForm";
+import UsersTable from "./components/table";
 import "./styles.css";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+  const [createFormVisible, setCreateFormVisible] = useState(false);
+
+  const addUser = user => {
+    setUsers([...users, user]);
+    setCreateFormVisible(false);
+  }
+
+  const deleteUser = userIndex => {
+    const newUsers = [...users];
+    newUsers.splice(userIndex, 1);
+    setUsers(newUsers);
+  };
+
+  const showCreateForm = () => setCreateFormVisible(true);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      { createFormVisible ? 
+        <CreateForm onCreateUser={addUser} /> :
+        <>
+        { 
+          users.length === 0 ?
+            <p>There are no users introduced.</p> :
+            <UsersTable users={users} onDeleteUser={deleteUser} />
+        }<br/><br/>
+        <button onClick={showCreateForm}>Add new user</button>
+      </> 
+    }
     </div>
   );
 }
